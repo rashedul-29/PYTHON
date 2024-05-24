03aa12136712a8934ed7cde3d3e6333d9fe1e1a3
rashedul-29 <rhasnat2412.rhs@gmail.com>
2024-05-24 11:15:22 +0200

    Practising the use of if else

---
+++ script.py
@@ -0,0 +1,16 @@
+#For reading an integer from the input given
+if __name__ == '__main__':
+    n = int(input().strip())
+    
+#I am checking if the integer is odd or not in "if" condition. "Else" is for the opposite.
+
+    if n % 2 == 1:
+        print("Weird")
+
+    else: 
+        if 2 <= n <= 5:
+            print("Not Weird")
+        elif 6 <= n <= 20:
+            print("Weird")
+        elif n > 20:
+            print("Not Weird")
